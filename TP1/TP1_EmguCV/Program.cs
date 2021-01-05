aa5fce0ca7d3b399a24a3271da29d940641b950d
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-01-05 11:51:21 +0100

    Changed single channel copy function to Bgr color type

--- TP1/TP1_EmguCV/Program.cs
+++ TP1/TP1_EmguCV/Program.cs
@@ -9,9 +9,27 @@ namespace TP1_EmguCV {
     class Program {
         static void Main(string[] args) {
 
-            Exercice3();
+            Exercice4();
 
         }
+
+        //static void Exercice4() {
+
+        //    String window = "TP1";
+        //    CvInvoke.NamedWindow(window);
+
+        //    Mat matBase = new Mat("..\\..\\..\\..\\images\\crochet.jpg"); //Because it is located in directory "netcoreapp3.1"
+
+        //    Mat matGrey = new Mat(matBase.Width, matBase.Height, )
+
+        //    CvInvoke.CvtColor(baseImg, baseGrayImg);
+
+
+        //    CvInvoke.Imshow(window, matRes);
+        //    CvInvoke.WaitKey(0);  //Wait for the key pressing event
+        //    CvInvoke.DestroyWindow(window); //Destroy the window if key is pressed
+
+        //}
 
         static void Exercice3() {
 
@@ -22,13 +40,12 @@ namespace TP1_EmguCV {
 
             Image<Bgr, Byte> baseImg = matWebcam.ToImage<Bgr, Byte>();
             Image<Gray, Byte> grayScaleBaseImg = matWebcam.ToImage<Gray, Byte>();
-            Image<Hsv, Byte> hsvScaleBaseImg = matWebcam.ToImage<Hsv, Byte>();
 
             Image<Bgr, Byte> resultImage = new Image<Bgr, byte>(baseImg.Width * 3, baseImg.Height);
 
             CopyToImage(ref baseImg, ref resultImage, 0, 0);
             CopyToImage(ref grayScaleBaseImg, ref resultImage, baseImg.Width, 0);
-            GrayCopyChannelToImage(ref hsvScaleBaseImg, ref resultImage, baseImg.Width * 2, 0, 0);
+            GrayCopyChannelToImage(ref baseImg, ref resultImage, baseImg.Width * 2, 0, 0);
 
             Mat matRes = resultImage.Mat;
 
@@ -69,7 +86,7 @@ namespace TP1_EmguCV {
         }
 
         // Gray to BGR
-        static void GrayCopyChannelToImage(ref Image<Hsv, Byte> inputImg, ref Image<Bgr, Byte> outputImg, int offsetX, int offsetY, int channel) {
+        static void GrayCopyChannelToImage(ref Image<Bgr, Byte> inputImg, ref Image<Bgr, Byte> outputImg, int offsetX, int offsetY, int channel) {
 
             for (int i = 0; i < inputImg.Height; i++) {
                 for (int j = 0; j < inputImg.Width; j++) {
