ac1b5abecad0d31603fb3b9924fa95428c6abfac
NicolasCalvet <nicolas.calvetgp@gmail.com>
2021-01-05 11:05:26 +0100

    Printing crochet.jpg image

--- TP1/TP1_EmguCV/Program.cs
+++ TP1/TP1_EmguCV/Program.cs
@@ -1,9 +1,23 @@
 ﻿using System;
 
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+
 namespace TP1_EmguCV {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            String window = "TP1";
+            CvInvoke.NamedWindow(window);
+
+            Mat matWebcam = new Mat("..\\..\\..\\..\\images\\crochet.jpg"); //Because it is located in directory "netcoreapp3.1"
+
+            CvInvoke.Imshow(window, matWebcam);
+            CvInvoke.WaitKey(0);  //Wait for the key pressing event
+            CvInvoke.DestroyWindow(window); //Destroy the window if key is pressed
+
         }
     }
 }
